bd399c818728ae364f436810407db2084f9c9ca0
chenyichao <18221659878@163.com>
2016-05-31 13:05:56 +0800

    baidugeo

---
+++ assets/js/baidugeo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by beetle on 2016/5/31.
+ */
+
+var baidugeo = function () {
+    this.url = "";
+    this.isGeoSupport = function () {
+        return navigator.geolocation;
+    }
+};
