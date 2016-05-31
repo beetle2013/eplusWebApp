e08edf4133809ffeb7617ad14d471d7e6ee2619f
chenyichao <18221659878@163.com>
2016-05-31 18:03:29 +0800

    googlegeo

--- assets/js/baidugeo.js
+++ assets/js/baidugeo.js
@@ -10,6 +10,9 @@ function geo() {
     var baidu_url = "http://api.map.baidu.com/geocoder/v2/";
     var baidu_ak = "sk4aD5OWDtIzYAgtOD8PdAmW";
 
+    var formatted_address;
+    var business;
+    var addressComponent;
 
     instance.htmlgeo = function () {
         if (navigator.geolocation) {
@@ -37,7 +40,9 @@ function geo() {
             },
             success: function (data) {
                 if (data.status == 0) {
-                    alert(data.result.business);
+                    formatted_address = data.result.formatted_address;
+                    business = data.result.business;
+                    addressComponent = data.result.addressComponent;
                 }
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -46,5 +51,29 @@ function geo() {
         });
     }
 
+    function googlegeo(){
+
+        var url = 'http://maps.google.cn/maps/api/geocode/json?latlng='+latitude + "," + longitude +'&language=CN';
+        $.ajax({
+            type: "GET",
+            url: url,
+            beforeSend: function(){
+                alert('正在定位...');
+            },
+            success: function (json) {
+                if(json.status=='OK'){
+                    var results = json.results;
+                    $.each(results,function(index,array){
+                        if(index==0){
+                            alert(array['formatted_address']);
+                        }
+                    });
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert(latlon+"地址位置获取失败");
+            }
+        });
+    }
     return instance;
 }
